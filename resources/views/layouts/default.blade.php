0b8d61054ae09b5eaa2d9e7feb7b4634db2939a1
Gabriel Batista <ryubiel@gmail.com>
2025-10-08 17:02:34 -0300

    Layouts

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+<!--begin::Head-->
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>AdminLTE v4 | Dashboard</title>
+    <!--begin::Accessibility Meta Tags-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes"/>
+    <meta name="color-scheme" content="light dark"/>
+    <meta name="theme-color" content="#007bff" media="(prefers-color-scheme: light)"/>
+    <meta name="theme-color" content="#1a1a1a" media="(prefers-color-scheme: dark)"/>
+    <!--end::Accessibility Meta Tags-->
+    <!--begin::Primary Meta Tags-->
+    <meta name="title" content="AdminLTE v4 | Dashboard"/>
+    <meta name="author" content="ColorlibHQ"/>
+    <meta
+        name="description"
+        content="AdminLTE is a Free Bootstrap 5 Admin Dashboard, 30 example pages using Vanilla JS. Fully accessible with WCAG 2.1 AA compliance."
+    />
+    <meta
+        name="keywords"
+        content="bootstrap 5, bootstrap, bootstrap 5 admin dashboard, bootstrap 5 dashboard, bootstrap 5 charts, bootstrap 5 calendar, bootstrap 5 datepicker, bootstrap 5 tables, bootstrap 5 datatable, vanilla js datatable, colorlibhq, colorlibhq dashboard, colorlibhq admin dashboard, accessible admin panel, WCAG compliant"
+    />
+    <meta name="supported-color-schemes" content="light dark"/>
+    @vite('resources/scss/app.scss')
+</head>
+<!--end::Head-->
+<!--begin::Body-->
+<body class="layout-fixed sidebar-expand-lg sidebar-open bg-body-tertiary">
+@vite('resources/js/app.js')
+<!--begin::App Wrapper-->
+<div class="app-wrapper">
+    @include('parts.header')
+    @include('parts.side')
+    <!--begin::App Main-->
+    <main class="app-main">
+        <!--begin::App Content Header-->
+        @include('parts.content-header')
+        <!--end::App Content Header-->
+        <!--begin::App Content-->
+        <div class="app-content">
+            <!--begin::Container-->
+            <div class="container-fluid">
+                <!--begin::Row-->
+                @yield('content')
+                <!-- /.row (main row) -->
+            </div>
+            <!--end::Container-->
+        </div>
+        <!--end::App Content-->
+    </main>
+    <!--end::App Main-->
+    @include('parts.footer')
+</div>
+
+</body>
+{{--<!--end::Body-->--}}
+</html>
---
+++ resources/views/parts/content-header.blade.php
@@ -0,0 +1,27 @@
+<div class="app-content-header">
+    <!--begin::Container-->
+    <div class="container-fluid">
+        <!--begin::Row-->
+        <div class="row">
+            <div class="col-sm-6">
+                @hasSection('page-title')
+                <h3 class="mb-0">@yield('page-title')</h3>
+                @endif
+
+                @isset($breadcrumbs)
+                <ol class="breadcrumb">
+                    @foreach($breadcrumbs as $breadcrumb)
+                    <li class="breadcrumb-item"><a href="#">{{ $breadcrumb['label'] }}</a></li>
+{{--                    <li class="breadcrumb-item active" aria-current="page">Dashboard</li>--}}
+                    @endforeach
+                </ol>
+                @endisset
+            </div>
+            <div class="col-sm-6">
+                Actions
+            </div>
+        </div>
+        <!--end::Row-->
+    </div>
+    <!--end::Container-->
+</div>
