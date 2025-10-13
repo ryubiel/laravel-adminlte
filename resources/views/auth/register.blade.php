2f9938c7af7c0a3712644093498169ccaa2e4e97
Gabriel Batista <ryubiel@gmail.com>
2025-10-13 19:30:36 -0300

    Register Page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,34 +3,52 @@
 @section('content')
     <div class="register-box">
         <div class="register-logo">
-            <a href="../index2.html"><b>Admin</b>LTE</a>
+            <a href="../index3.html"><b>Admin</b>LTE</a>
         </div>
-        <!-- /.register-logo -->
         <div class="card">
             <div class="card-body register-card-body">
                 <p class="register-box-msg">Register a new membership</p>
-                <form action="../index3.html" method="post">
+                <form action="{{ route('register') }}" method="post">
+                    @csrf
                     <div class="input-group mb-3">
-                        <input type="text" class="form-control" placeholder="Full Name" />
+                        <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" placeholder="Full Name" value="{{ old('name') }}"/>
                         <div class="input-group-text"><span class="bi bi-person"></span></div>
+                        @error('name')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="input-group mb-3">
-                        <input type="email" class="form-control" placeholder="Email" />
+                        <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" value="{{ old('email') }}"/>
                         <div class="input-group-text"><span class="bi bi-envelope"></span></div>
+                        @error('email')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="input-group mb-3">
-                        <input type="password" class="form-control" placeholder="Password" />
+                        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" />
+                        <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
+                        @error('password')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" name="password_confirmation" class="form-control" placeholder="Password Confirmation" />
                         <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
                     </div>
                     <div class="d-grid gap-2">
-                        <button type="submit" class="btn btn-primary">Sign In</button>
+                        <button type="submit" class="btn btn-primary">Register</button>
                     </div>
                 </form>
                 <p class="mb-0 text-center">
                     <a href="login.html" class="text-center"> I already have a membership </a>
                 </p>
             </div>
-            <!-- /.register-card-body -->
         </div>
     </div>
 @endsection
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,16 +1,12 @@
 <!doctype html>
 <html lang="en">
-<!--begin::Head-->
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>AdminLTE 4 | Register Page</title>
-    <!--begin::Accessibility Meta Tags-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
     <meta name="color-scheme" content="light dark" />
     <meta name="theme-color" content="#007bff" media="(prefers-color-scheme: light)" />
     <meta name="theme-color" content="#1a1a1a" media="(prefers-color-scheme: dark)" />
-    <!--end::Accessibility Meta Tags-->
-    <!--begin::Primary Meta Tags-->
     <meta name="title" content="AdminLTE 4 | Register Page" />
     <meta name="author" content="ColorlibHQ" />
     <meta
