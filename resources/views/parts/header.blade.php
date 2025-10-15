0867c3404bc22f9c1634e7ed1f9047b691c94f4f
Gabriel Batista <ryubiel@gmail.com>
2025-10-15 15:44:29 -0300

    Logout

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -158,29 +158,22 @@
                             <small>Member since {{ auth()->user()->created_at->translatedFormat('M. Y') }}</small>
                         </p>
                     </li>
-                    <!--end::User Image-->
-                    <!--begin::Menu Body-->
                     <li class="user-body">
-                        <!--begin::Row-->
                         <div class="row">
                             <div class="col-4 text-center"><a href="#">Followers</a></div>
                             <div class="col-4 text-center"><a href="#">Sales</a></div>
                             <div class="col-4 text-center"><a href="#">Friends</a></div>
                         </div>
-                        <!--end::Row-->
                     </li>
-                    <!--end::Menu Body-->
-                    <!--begin::Menu Footer-->
                     <li class="user-footer">
+                        <form action="{{ route('logout') }}" method="POST">
+                            @csrf
+                            <button type="submit" class="btn btn-default btn-flat float-end">Logout</button>
+                        </form>
                         <a href="#" class="btn btn-default btn-flat">Profile</a>
-                        <a href="#" class="btn btn-default btn-flat float-end">Sign out</a>
                     </li>
-                    <!--end::Menu Footer-->
                 </ul>
             </li>
-            <!--end::User Menu Dropdown-->
         </ul>
-        <!--end::End Navbar Links-->
     </div>
-    <!--end::Container-->
 </nav>
