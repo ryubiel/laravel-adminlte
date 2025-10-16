6780d2aa86e43868f0dd1538fd22867f5e964504
Gabriel Batista <ryubiel@gmail.com>
2025-10-16 15:59:09 -0300

    Add users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,4 +11,20 @@ class UserController extends Controller
         $users = User::all();
         return view('users.index', compact('users'));
     }
+
+    public function create() {
+        return view('users.create');
+    }
+
+    public function store(Request $request) {
+        $input = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+
+        User::create($input);
+
+        return redirect()->route('users.index')->with('status', 'Usuário adicionado com sucesso!');
+    }
 }
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.default')
+@section('page-title', 'Adicionar Usuário')
+@section('content')
+    <form action="{{ route('users.store') }}" method="POST">
+        @csrf
+        <div class="mb-3">
+            <label class="form-label">Nome</label>
+            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}">
+            @error('name')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label">Email</label>
+            <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}">
+            @error('email')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label">Senha</label>
+            <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"">
+            @error('password')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
+        </div>
+
+        <button type="submit" class="btn btn-primary">Adicionar</button>
+    </form>
+@endsection
