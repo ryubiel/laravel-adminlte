5776d7965d43cc54062dab5ac264341651142238
Gabriel Batista <ryubiel@gmail.com>
2025-10-31 14:03:15 -0300

    Permission policys

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -10,40 +10,45 @@
         </div>
     @endsession
 
+    
     <form action="{{ route('users.index') }}" method="GET" class="mb-3">
-        <div class="input-group input-group-sm">
-            <input type="text" name="keyword" class="form-control" placeholder="Pesquise por nome ou email" value="{{ request()?->keyword }}">
+        <div class="input-group input-group-sm w-25">
+            <input type="text" name="keyword" class="form-control me-1" placeholder="Pesquise por nome ou email" value="{{ request()?->keyword }}">
             <button type="submit" class="btn btn-primary">Pesquisar</button>
         </div>
     </form>
-
+    
     <table class="table">
         <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Nome</th>
-            <th scope="col">E-mail</th>
-            <th scope="col">Ação</th>
-        </tr>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nome</th>
+                <th scope="col">E-mail</th>
+                <th scope="col">Ação</th>
+            </tr>
         </thead>
         <tbody>
-        @foreach($users as $user)
+            @foreach($users as $user)
             <tr>
                 <th scope="row">{{ $user->id }}</th>
                 <td>{{ $user->name }}</td>
                 <td>{{ $user->email }}</td>
                 <td class="d-flex gap-2">
-                    <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary btn-sm">Editar</a>
-                    <form action="{{ route('users.destroy', $user->id) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger btn-sm">Excluir</button>
-                    </form>
+                    @can('edit', \App\Models\User::class)
+                        <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary btn-sm">Editar</a>
+                    @endcan
+                    @can('destroy', \App\Models\User::class)
+                        <form action="{{ route('users.destroy', $user->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btn-sm">Excluir</button>
+                        </form>
+                    @endcan
                 </td>
             </tr>
-        @endforeach
+            @endforeach
         </tbody>
     </table>
-
+    
     {{ $users->links() }}
 @endsection
